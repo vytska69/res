35dd42397329a58e00d70bb7d09b7ffdf17a05e4
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2021-11-06 13:31:37 +0530

    pine: Don't remove DocumentsUIGoogle & FilesPrebuilt package

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := DocumentsUIGoogle MarkupGoogle CarrierServices FilesPrebuilt SafetyHubPrebuilt MusicPlayerGO 
+LOCAL_OVERRIDES_PACKAGES := MarkupGoogle CarrierServices SafetyHubPrebuilt MusicPlayerGO 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
